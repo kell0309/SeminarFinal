39107604bb5d72a244cc26496f1069b337d671fd
Khalif Hussein <a23khahu@student.his.se>
2025-12-17 10:31:51 +0100

    add enrichment, plotting, filtering and excel scripts

--- final/R/Filter_low_expressions.R
+++ final/R/Filter_low_expressions.R
@@ -1,3 +1,13 @@
+#'Filters lowly expressed genes and creates a normalised list using edgeR and DGElist
+
+#' #´@description
+#' This code aligns the sample metadata and the count matrix, also removes lowly expressed genes.
+#'
+#' @param counts A data frame of raw counts
+#' @samples a data frame with sample table
+#' @group col a column names with in samples
+
+#' @return
 filter_low_expression <- function(counts, samples, group_col = "disease") {
   samples <- samples[match(colnames(counts), samples$sample), ]
   group <- samples[[group_col]]
---
+++ final/R/excel_table.R
@@ -0,0 +1,17 @@
+#' @description
+#' Writes a differential expression results table to an excel file using `openxlsx`.
+#' The output contains one sheet named "DEG"
+#'
+#' @param deg_table A data.frame  containing DEG results to export.
+#' @param out_xlsx  Output Excel filename or path
+#'
+#' @return  The path/filename of the saved Excel file.
+
+export_deg_excel <- function(deg_table, out_xlsx = "DEG_results.xlsx") {
+  wb <- createWorkbook()
+  addWorksheet(wb, "DEG")
+  writeData(wb, "DEG", deg_table)
+  freezePane(wb, "DEG", firstRow = TRUE)
+  saveWorkbook(wb, out_xlsx, overwrite = TRUE)
+  out_xlsx
+}
